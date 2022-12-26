f8ec35239d5259708b3c6b2d46a4dac2fe847e18
Ayush Singh <ayush.singh21082@gmail.com>
2022-12-26 21:16:59 +0530

    Initial Commit

---
+++ Retro Calculator/Retro_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retro_CalculatorApp.swift
+//  Retro Calculator
+//
+//  Created by Ayush Singh on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Retro_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
